1d0554f4342c526bf2a0c5c4121af303f2ef587e
cy-Yin <78548391+cy-Yin@users.noreply.github.com>
2022-04-22 21:42:37 +0800

    Add files via upload

---
+++ FlightFight/supply.py
@@ -0,0 +1,60 @@
+import pygame
+from random import *
+
+
+# define the supply of double bullets per second
+class BUllet_Supply(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(r"images\bullet_supply.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.bottom = randint(0, self.width-self.rect.width), -100
+
+        self.width, self.height = bg_size[0], bg_size[1]
+
+        self.speed = 5
+        # check whether it is time to give the supply item and whether it is available
+        self.active = False
+        # to use sprite.collide_mask function to check the collision more accurately
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top += self.speed
+        else:
+            self.active = False
+
+    def reset(self):
+        self.active = True
+        self.rect.left, self.rect.bottom = randint(0, self.width-self.rect.width), -100
+
+
+# define the supply of full-screen bombs
+class Bomb_Supply(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(r"images\bomb_supply.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.bottom = randint(0, self.width-self.rect.width), -100
+
+        self.width, self.height = bg_size[0], bg_size[1]
+
+        self.speed = 5
+        # check whether it is time to give the supply item and whether it is available
+        self.active = False
+        # to use sprite.collide_mask function to check the collision more accurately
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top += self.speed
+        else:
+            self.active = False
+
+    def reset(self):
+        self.active = True
+        self.rect.left, self.rect.bottom = randint(0, self.width-self.rect.width), -100
